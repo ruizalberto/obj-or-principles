02a709ff26ae93b4eb209c0c65e9925f5102a405
ruizalberto <albertoruizb14@gmail.com>
2022-09-23 11:47:18 +0100

    Made ex2

--- labs/week1/src/MyMain.java
+++ labs/week1/src/MyMain.java
@@ -66,7 +66,9 @@ public class MyMain {
 		// COMPLETE YOUR CODE HERE
 		//----------------------------
 
-
+		if (a>b && a<c){System.out.println(a);}
+		else if (b>a && b<c){System.out.println(b);}
+		else {System.out.println(c);}
 
 		//----------------------------
 	}
