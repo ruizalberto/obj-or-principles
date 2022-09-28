92db8d2063761f467793326f106f1ee2e96507e2
ruizalberto <albertoruizb14@gmail.com>
2022-09-28 17:42:26 +0100

    Made constructor for Dog class

--- labs/week2/src/Dog.java
+++ labs/week2/src/Dog.java
@@ -5,13 +5,19 @@ public class Dog {
     //	Fields
     //---------------------------------------
 
-
+    String name;
+    int boldnessLevel;
+    String breed;
 
     //---------------------------------------
     //	Constructor
     //---------------------------------------
 
-
+    public Dog(String myName, int myBoldnessLevel, String myBreed) {
+        this.name = myName;
+        this.boldnessLevel = myBoldnessLevel;
+        this.breed = myBreed;
+    }
 
     //---------------------------------------
     //	Get Methods
