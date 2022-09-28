db8e777300b7043a22169680277c5783b73aa34d
ruizalberto <albertoruizb14@gmail.com>
2022-09-28 17:55:07 +0100

    Finished Cat

--- labs/week2/src/Cat.java
+++ labs/week2/src/Cat.java
@@ -17,26 +17,44 @@ public class Cat {
         this.playfulLevel = myPlayfulLevel;
     }
 
-
     //---------------------------------------
     //	Get Methods
     //---------------------------------------
 
+    public String getName() {
+        return name;
+    }
 
-
+    public int getPlayfulLevel() {
+        return playfulLevel;
+    }
 
     //---------------------------------------
     //	Set Method
     //---------------------------------------
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-
+    public void setPlayfulLevel(int playfulLevel) {
+        this.playfulLevel = playfulLevel;
+    }
 
     //---------------------------------------
     //	Extra Functionality
     //---------------------------------------
 
+    public boolean isPayingAttention(){
+        int N = 1;
+        int M = 3;
+        int valorEntero = (int) Math.floor(Math.random()*(N-M+1)+M);
 
+        if (valorEntero==3){
+            return true;
+        }
+        return false;
+    }
 
     
 
