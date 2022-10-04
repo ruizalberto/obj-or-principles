4c2c52e9ecd2f9396e32aca2cd89bfb9cd12a87a
ruizalberto <albertoruizb14@gmail.com>
2022-10-04 16:46:57 +0100

    Made week 3 lab

--- labs/week3/Module.java
+++ labs/week3/Module.java
@@ -39,11 +39,54 @@ public class Module {
     //	Set Methods
     //---------------------------------------
 
+    public void setIsCA(boolean b) {
+        this.isCA = b;
+    }
 
+    public void setCRN(int i) {
+        this.CRN = i;
+    }
+
+    public void setTitle(String s) {
+        this.title = s;
+    }
 
     //---------------------------------------
     //	Extra Functionality
     //---------------------------------------
 
-    
+    public void printModuleInfo(int option){
+        if (option == 1){
+            System.out.println(getTitle());
+        } else if (option == 2){
+            System.out.println(getCRN());
+        } else if (option == 3){
+            if (getIsCA()){
+                System.out.println("Assignment-based");
+            } else {
+                System.out.println("Exam-based");
+            }
+        }
+    }
+
+    public int numVowelsOnTitle() {
+        int count = 0;
+        for (int i=0 ; i<this.title.length(); i++){
+            char ch = this.title.charAt(i);
+            if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'){
+               count++;
+            }
+         }
+         return count;
+    }
+
+    public int sumOfCRNDigits() {
+        int num = this.CRN;
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }    
 }
