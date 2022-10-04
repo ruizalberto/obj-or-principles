4edcc4e435166ae97d4d2e295161f0a2bd24f921
ruizalberto <albertoruizb14@gmail.com>
2022-10-04 16:24:59 +0100

    Get methods

--- labs/week3/Module.java
+++ labs/week3/Module.java
@@ -5,19 +5,35 @@ public class Module {
     //	Fields
     //---------------------------------------
 
-
+    String title;
+    int CRN;
+    boolean isCA;
 
     //---------------------------------------
     //	Constructor
     //---------------------------------------
 
-
+    public Module(String s, int i, boolean b){
+        this.title = s;
+        this.CRN = i;
+        this.isCA = b;
+    }
 
     //---------------------------------------
     //	Get Methods
     //---------------------------------------
 
+    public int getCRN() {
+        return this.CRN;
+    }
 
+    public String getTitle() {
+        return this.title;
+    }
+
+    public boolean getIsCA() {
+        return this.isCA;
+    }
 
     //---------------------------------------
     //	Set Methods
