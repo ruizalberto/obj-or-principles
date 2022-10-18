bc29d6cc1e76b834cf06502b65bfea50a268edd0
ruizalberto <albertoruizb14@gmail.com>
2022-10-18 16:46:32 +0100

    Finish Cat and GoldenRetriever

--- labs/week5/src/Cat.java
+++ labs/week5/src/Cat.java
@@ -10,33 +10,48 @@ public class Cat extends Animal {
     //	Fields
     //---------------------------------------
 
-
+    protected int attentionLevel;
 
 
     //---------------------------------------
     //	Constructor
     //---------------------------------------
 
-
+    public Cat(String _name, int _age, int _attentionLevel){
+        super(_name, _age);
+        this.attentionLevel = _attentionLevel;
+    }
 
 
     //---------------------------------------
     //	Get Methods
     //---------------------------------------
 
-
+    public int getAttentionLevel() {
+        return attentionLevel;
+    }
 
 
     //---------------------------------------
     //	Set Method
     //---------------------------------------
 
-
+    public void setAttentionLevel(int attentionLevel) {
+        this.attentionLevel = attentionLevel;
+    }
 
 
     //---------------------------------------
     //	Extra Functionality
     //---------------------------------------
 
+    public boolean isPayingAttention(){
+        Random ran = new Random();
+        int coin = ran.nextInt(9)+1;
+        if (coin <= getAttentionLevel()){
+            return true;
+        }
+        return false;
+    }
 
 }
--- labs/week5/src/GoldenRetriever.java
+++ labs/week5/src/GoldenRetriever.java
@@ -10,33 +10,47 @@ public class GoldenRetriever extends Dog {
     //	Fields
     //---------------------------------------
 
-
+    protected int lazinessLevel;
 
 
     //---------------------------------------
     //	Constructor
     //---------------------------------------
 
-
+    public GoldenRetriever(String _name, int _age, int _boldnessLevel, int _lazinessLevel){
+        super(_name, _age, _boldnessLevel);
+        this.lazinessLevel = _lazinessLevel;
+    }
+    public GoldenRetriever(String _name, int _age, int _boldnessLevel, DogToy _myToy, int _lazinessLevel){
+        super(_name, _age, _boldnessLevel, _myToy);
+        this.lazinessLevel = _lazinessLevel;
+    }
 
 
     //---------------------------------------
     //	Get Methods
     //---------------------------------------
 
-
+    public int getLazinessLevel() {
+        return lazinessLevel;
+    }
 
 
     //---------------------------------------
     //	Set Method
     //---------------------------------------
 
-
+    public void setLazinessLevel(int lazinessLevel) {
+        this.lazinessLevel = lazinessLevel;
+    }
 
 
     //---------------------------------------
     //	Extra Functionality
     //---------------------------------------
 
-
+    public int hoursLazy(){
+        Random ran = new Random();
+        return ran.nextInt(24)+1;
+    }
 }
