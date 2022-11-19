62e09ea9ec78f9e42976219e633daa301572babb
ruizalberto <albertoruizb14@gmail.com>
2022-11-19 13:18:03 +0000

    Add files for university online project

---
+++ labs/assignments/A03/A03_Student_Java_Application/src/Module.java
@@ -0,0 +1,17 @@
+public class Module {
+    // Fields
+    private String moduleID;
+    private int teacherID;
+    private String moduleDescriptor;
+
+    // Constructors
+    public Module(){
+        this.moduleID = null;
+        this.teacherID = 0;
+        this.moduleDescriptor = null;
+    }
+
+    public Module(String _moduleID, int _teacherID, String _moduleDescriptor){
+        
+    }
+}
