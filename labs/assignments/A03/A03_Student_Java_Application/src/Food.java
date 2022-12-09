0ccc1deb97ea6c830b81e1b12a3b5c56894cf4cd
ruizalberto <albertoruizb14@gmail.com>
2022-12-09 21:45:14 +0000

    Make some javadoc for food java class

--- labs/assignments/A03/A03_Student_Java_Application/src/Food.java
+++ labs/assignments/A03/A03_Student_Java_Application/src/Food.java
@@ -1,15 +1,37 @@
+//--------------------------------------------------
+//	IMPORTS
+//--------------------------------------------------
+
 import java.time.LocalDate;
 import java.util.UUID;
 
+//--------------------------------------------------
+//
+//	CLASS Food
+//
+//--------------------------------------------------
+/**
+ * This class models food for the shop menu<br>.
+ */
 public class Food {
-    // Fields
+
+    //---------------------------------------
+    //	Fields
+    //---------------------------------------
     private String foodID,
     name;
     private LocalDate bestBefore;
     private double price;
     private int quantity;
     
-    // Constructors
+    //---------------------------------------
+    //	Constructor
+    //---------------------------------------
+    /**
+     * The constructor creates 1 instance (1 object) of the class Food<br>
+     * @param _name - The name of the food.
+     * @param _price - The price of the food.
+     */
     public Food(String _name, double _price){
         this.name = _name;
         this.foodID = UUID.randomUUID().toString();
@@ -17,39 +39,79 @@ public class Food {
         this.price = _price;
     }
 
-    // GET Methods
+    //---------------------------------------
+    //	GET METHODS
+    //---------------------------------------
+    /** 
+     * Given a concrete food (this), the function returns its name.<br>
+     * @return The name of the food.
+     */
     public String getName() {
         return this.name;
     }
+    
+    /** 
+     * @return LocalDate
+     */
     public LocalDate getBestBefore() {
         return this.bestBefore;
     }
+    
+    /** 
+     * @return String
+     */
     public String getFoodID() {
         return this.foodID;
     }
+    
+    /** 
+     * @return double
+     */
     public double getPrice() {
         return this.price;
     }
+    
+    /** 
+     * @return int
+     */
     public int getQuantity() {
         return this.quantity;
     }
 
-    // SET Methods
+
+    
+    /** 
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
+    
+    /** 
+     * @param bestBefore
+     */
     public void setBestBefore(LocalDate bestBefore) {
         this.bestBefore = bestBefore;
     }
+    
+    /** 
+     * @param foodID
+     */
     public void setFoodID(String foodID) {
         this.foodID = foodID;
     }
+    
+    /** 
+     * @param price
+     */
     public void setPrice(int price) {
         this.price = price;
     }
+    
+    /** 
+     * @param quantity
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-
-    // EXTRA Methods
 }
