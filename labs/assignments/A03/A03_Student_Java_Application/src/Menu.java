810bbf979ce749abb6c2a748a341032da7de0207
ruizalberto <albertoruizb14@gmail.com>
2022-12-10 20:41:48 +0000

    Finish javadoc for menu java class

--- labs/assignments/A03/A03_Student_Java_Application/src/Menu.java
+++ labs/assignments/A03/A03_Student_Java_Application/src/Menu.java
@@ -1,9 +1,27 @@
+//--------------------------------------------------
+//	IMPORTS
+//--------------------------------------------------
+
 import java.util.ArrayList;
 
+//--------------------------------------------------
+//
+//	CLASS Menu
+//
+//--------------------------------------------------
+/**
+ * This class models a Menu of the shop<br>.
+ */
 public abstract class Menu {
-    // Fields
+
+    //---------------------------------------
+    //	Fields
+    //---------------------------------------
     private ArrayList<Food> menu;
 
+    //---------------------------------------
+    //	Constructor
+    //---------------------------------------
     /**
      * The constructor creates 1 instance (1 object) of the class Menu<br>
      */
@@ -11,24 +29,33 @@ public abstract class Menu {
         this.menu = new ArrayList<Food>();
     }
 
-    
+    //---------------------------------------
+    //	GET METHODS
+    //---------------------------------------    
     /** 
-     * @return ArrayList<Food>
+     * Given a concrete menu (this), the function returns its menu.<br>
+     * @return The menu.
      */
-    // GET Methods
     public final ArrayList<Food> getMenu() {
         return this.menu;
     }
 
-    
+    //---------------------------------------
+    //	SET METHODS
+    //---------------------------------------
     /** 
+     * Given a concrete menu (this), the function updates its menu.<br>
      * @param menu
      */
-    // SET Methods
     public void setMenu(ArrayList<Food> menu) {
         this.menu = menu;
     }
 
-    // EXTRA Methods
+    //---------------------------------------
+    //	EXTRA METHODS
+    //---------------------------------------
+    /** 
+     * This method displays the menu.<br>
+     */
     public abstract void displayMenu();
 }
